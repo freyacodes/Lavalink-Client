544dcf800516e00ea2100c6b3db4b0b1c19220b2
Duncan Sterken <duncan.sterken123@gmail.com>
2020-02-08 12:46:42 +0100

    Remove usages of method that will be removed in the future

---
+++ src/main/java/lavalink/client/io/GuildUnavailableException.java
@@ -0,0 +1,8 @@
+package lavalink.client.io;
+
+public class GuildUnavailableException extends RuntimeException {
+
+    public GuildUnavailableException(String message) {
+        super(message);
+    }
+}

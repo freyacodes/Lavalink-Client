ca922bb0ea22aa9869dcc0044130d594c017c65f
ByteAlex <bytealex@zerotwo.bot>
2020-04-26 16:57:22 +0200

    D4J impl

---
+++ src/main/java/lavalink/client/io/jda/D4JLavalink.java
@@ -0,0 +1,119 @@
+package lavalink.client.io.jda;
+
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.VoiceServerUpdateEvent;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.event.domain.channel.VoiceChannelDeleteEvent;
+import discord4j.core.event.domain.guild.GuildDeleteEvent;
+import discord4j.core.event.domain.lifecycle.ReconnectEvent;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import discord4j.discordjson.possible.Possible;
+import discord4j.rest.util.Snowflake;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import lavalink.client.io.Lavalink;
+import lavalink.client.io.Link;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+public class D4JLavalink extends Lavalink<D4JLink> {
+
+    private static final Logger log = LoggerFactory.getLogger(D4JLavalink.class);
+
+    private final GatewayDiscordClient client;
+    private boolean autoReconnect = true;
+
+    public D4JLavalink(final String userId, final int numShards, final GatewayDiscordClient client) {
+        super(userId, numShards);
+        this.client = client;
+    }
+
+    public boolean isAutoReconnect() {
+        return autoReconnect;
+    }
+
+    public void setAutoReconnect(boolean autoReconnect) {
+        this.autoReconnect = autoReconnect;
+    }
+
+    public GatewayDiscordClient getGateway() {
+        return client;
+    }
+
+    @NonNull
+    public D4JLink getLink(Guild guild) {
+        return getLink(guild.getId().asString());
+    }
+
+    @Nullable
+    public D4JLink getExistingLink(Guild guild) {
+        return getExistingLink(guild.getId().asString());
+    }
+
+    public Mono<Void> registerEvents() {
+        final Mono<Void> reconnectEvent = client.on(ReconnectEvent.class)
+                .filter(event -> isAutoReconnect())
+                .flatMap(event -> Flux.fromIterable(getLinks()))
+                .filter(entry -> entry.getLastChannel() != null)
+                .flatMap(entry -> Mono.zip(Mono.just(entry), client.getGuildById(Snowflake.of(entry.getGuildId()))))
+                .flatMap(tuple -> Mono.zip(Mono.just(tuple.getT1()),
+                        tuple.getT2().getChannelById(Snowflake.of(tuple.getT1().getLastChannel()))
+                                .cast(VoiceChannel.class)))
+                .doOnNext(tuple -> tuple.getT1().connect(tuple.getT2(), false))
+                .then();
+
+        final Mono<Void> guildDeleteEvent = client.on(GuildDeleteEvent.class)
+                .map(event -> getExistingLink(event.getGuildId().asString()))
+                .doOnNext(D4JLink::removeConnection)
+                .then();
+
+        final Mono<Void> channelDeleteEvent = client.on(VoiceChannelDeleteEvent.class)
+                .flatMap(event -> Mono.zip(Mono.just(event),
+                        Mono.fromCallable(() -> getExistingLink(event.getChannel().getGuildId().asString()))))
+                .filter(tuple -> tuple.getT1().getChannel().getId().equals(Snowflake.of(tuple.getT2().getLastChannel())))
+                .map(Tuple2::getT2)
+                .doOnNext(D4JLink::removeConnection)
+                .then();
+
+        final Mono<Void> voiceServerUpdateEvent = client.on(VoiceServerUpdateEvent.class)
+                .flatMap(event -> Mono.zip(Mono.just(event), event.getGuild()))
+                .flatMap(tuple -> Mono.zip(Mono.just(tuple.getT1()), tuple.getT2().getSelfMember()))
+                .flatMap(tuple -> Mono.zip(Mono.just(tuple.getT1()), tuple.getT2().getVoiceState()))
+                .flatMap(tuple -> Mono.zip(Mono.just(getLink(tuple.getT1().getGuildId().asString())),
+                        Mono.fromCallable(() -> new JSONObject()
+                                .put("token", tuple.getT1().getToken())
+                                .put("guild_id", tuple.getT1().getGuildId().asString())
+                                .put("endpoint", tuple.getT1().getEndpoint())),
+                        Mono.just(tuple.getT2().getSessionId())))
+                .doOnNext(triple -> triple.getT1().onVoiceServerUpdate(triple.getT2(), triple.getT3()))
+                .then();
+
+        final Mono<Void> voiceStateUpdateEvent = client.on(VoiceStateUpdateEvent.class)
+                .flatMap(event -> Mono.zip(
+                        event.getCurrent().getChannel().map(Possible::of).defaultIfEmpty(Possible.absent()),
+                        Mono.fromCallable(() -> getLink(event.getCurrent().getGuildId().asString()))))
+                .doOnNext(tuple -> {
+                    if (tuple.getT1().isAbsent()) {
+                        if (tuple.getT2().getState() != Link.State.DESTROYED) {
+                            tuple.getT2().onDisconnected();
+                        }
+                    } else {
+                        tuple.getT2().setChannel(tuple.getT1().get().getId().asString());
+                    }
+                })
+                .then();
+
+        return Mono.when(reconnectEvent, guildDeleteEvent, channelDeleteEvent, voiceServerUpdateEvent,
+                voiceStateUpdateEvent);
+    }
+
+    @Override
+    protected D4JLink buildNewLink(String guildId) {
+        return new D4JLink(this, guildId);
+    }
+}
---
+++ src/main/java/lavalink/client/io/jda/D4JLink.java
@@ -0,0 +1,124 @@
+package lavalink.client.io.jda;
+
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import discord4j.discordjson.json.gateway.VoiceStateUpdate;
+import discord4j.gateway.GatewayClientGroup;
+import discord4j.gateway.json.ShardGatewayPayload;
+import discord4j.rest.util.Permission;
+import discord4j.rest.util.PermissionSet;
+import discord4j.rest.util.Snowflake;
+import lavalink.client.io.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+public class D4JLink extends Link {
+
+    private static final Logger log = LoggerFactory.getLogger(D4JLink.class);
+    private final D4JLavalink lavalink;
+
+    D4JLink(final D4JLavalink lavalink, final String guildId) {
+        super(lavalink, guildId);
+        this.lavalink = lavalink;
+    }
+
+    public void connect(VoiceChannel voiceChannel) {
+        connect(voiceChannel, true);
+    }
+
+    void connect(VoiceChannel channel, boolean checkChannel) {
+        if (!channel.getGuildId().equals(Snowflake.of(getGuildId()))) {
+            throw new IllegalArgumentException("The provided VoiceChannel is not a part of the Guild that this " +
+                    "AudioManager handles." +
+                    "Please provide a VoiceChannel from the proper Guild");
+        }
+
+        final Guild guild = channel.getGuild().block();
+
+        if (guild == null) {
+            return;
+        }
+
+        if (guild.isUnavailable()) {
+            throw new IllegalStateException("Cannot open an Audio Connection with an unavailable guild. " +
+                    "Please wait until this Guild is available to open a connection.");
+        }
+
+        final Member self = guild.getSelfMember().block();
+
+        if (self == null) {
+            return;
+        }
+
+        final PermissionSet permissions = channel.getEffectivePermissions(self.getId()).block();
+
+        if (!permissions.contains(Permission.CONNECT) && !permissions.contains(Permission.MOVE_MEMBERS)) {
+            throw new IllegalStateException("Missing permission: " + Permission.CONNECT);
+        }
+
+        final Optional<Snowflake> selfChannel = self.getVoiceState().map(VoiceState::getChannelId).block();
+
+        //If we are already connected to this VoiceChannel, then do nothing.
+        if (checkChannel && selfChannel != null && selfChannel.isPresent() && selfChannel.get().equals(channel.getId())) {
+            return;
+        }
+
+        if (selfChannel != null && selfChannel.isPresent()) {
+            final int userLimit = channel.getUserLimit(); // userLimit is 0 if no limit is set!
+            /*if (!self.isOwner() && !self.hasPermission(Permission.ADMINISTRATOR)) {
+                if (userLimit > 0                                                      // If there is a userlimit
+                        && userLimit <= channel.getMembers().size()                    // if that userlimit is reached
+                        && !self.hasPermission(channel, Permission.VOICE_MOVE_OTHERS)) // If we don't have voice move
+                // others permissions
+                {
+                    throw new InsufficientPermissionException(channel, Permission.VOICE_MOVE_OTHERS, // then throw
+                            // exception!
+                            "Unable to connect to VoiceChannel due to userlimit! Requires permission " +
+                                    "VOICE_MOVE_OTHERS to bypass");
+                }
+            }
+             */
+        }
+
+        setState(State.CONNECTING);
+        queueAudioConnect(channel.getId().asLong());
+    }
+
+    @Override
+    protected void removeConnection() {
+    }
+
+    @Override
+    protected void queueAudioDisconnect() {
+        final GatewayClientGroup clientGroup = lavalink.getGateway().getGatewayClientGroup();
+        final long guildId = getGuildIdLong();
+        clientGroup.unicast(ShardGatewayPayload.voiceStateUpdate(VoiceStateUpdate.builder()
+                        .guildId(getGuildId())
+                        .selfDeaf(false)
+                        .selfMute(false)
+                        .build(),
+                (int) ((guildId >> 22) % clientGroup.getShardCount())))
+                .subscribe();
+    }
+
+    @Override
+    protected void queueAudioConnect(long channelId) {
+        lavalink.getGateway().getChannelById(Snowflake.of(channelId))
+                .ofType(VoiceChannel.class)
+                .flatMap(channel -> channel.getClient().getGatewayClientGroup().unicast(ShardGatewayPayload.voiceStateUpdate(
+                        VoiceStateUpdate.builder()
+                                .guildId(channel.getGuildId().asString())
+                                .channelId(channel.getId().asString())
+                                .selfDeaf(false)
+                                .selfMute(false)
+                                .build(),
+                        (int) ((channel.getId().asLong() >> 22) % channel.getClient().getGatewayClientGroup().getShardCount())
+                )))
+                .subscribe();
+    }
+
+}

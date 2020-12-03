eb26770e9bc75bc20c10850cd04e453465bb94be
Frederik Ar. Mikkelsen <fred@frederikam.com>
2020-12-03 13:27:59 +0100

    Fix client name

--- src/main/java/lavalink/client/io/Lavalink.java
+++ src/main/java/lavalink/client/io/Lavalink.java
@@ -108,7 +108,7 @@ public abstract class Lavalink<T extends Link> {
         headers.put("Authorization", password);
         headers.put("Num-Shards", Integer.toString(numShards));
         headers.put("User-Id", userId);
-        headers.put("Client-Name", "Client-Name");
+        headers.put("Client-Name", "Lavalink-Client");
 
         LavalinkSocket socket = new LavalinkSocket(name, this, serverUri, new Draft_6455(), headers);
         socket.connect();

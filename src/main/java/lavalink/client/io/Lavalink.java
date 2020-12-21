7492a36d200b9af8d4ff003b32f8bb5d01ea0858
Frederik Ar. Mikkelsen <fred@frederikam.com>
2020-12-21 01:48:20 +0100

    Log link destruction as debug

--- src/main/java/lavalink/client/io/Lavalink.java
+++ src/main/java/lavalink/client/io/Lavalink.java
@@ -183,7 +183,7 @@ public abstract class Lavalink<T extends Link> {
     }
 
     void removeDestroyedLink(Link link) {
-        log.info("Destroyed link for guild " + link.getGuildId());
+        log.debug("Destroyed link for guild " + link.getGuildId());
         links.remove(link.getGuildId());
     }
 

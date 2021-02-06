5082149a0124ab9a8544807262f8275ddd758925
TopiSenpai <tobi@strasserhof.com>
2021-02-06 12:31:02 +0100

    fixed incorrect selected track for playlists over rest client

--- src/main/java/lavalink/client/io/LavalinkRestClient.java
+++ src/main/java/lavalink/client/io/LavalinkRestClient.java
@@ -245,7 +245,7 @@ public final class LavalinkRestClient {
             }
 
             final JSONObject playlistInfo = loadResult.getJSONObject("playlistInfo");
-            final AudioTrack selectedTrack = tracks.get(0);
+	        final AudioTrack selectedTrack = playlistInfo.isNull("selectedTrack") ? null : tracks.get(playlistInfo.getInt("selectedTrack"));
             final String playlistName = playlistInfo.getString("name");
 
             return new BasicAudioPlaylist(playlistName, tracks, selectedTrack, false);

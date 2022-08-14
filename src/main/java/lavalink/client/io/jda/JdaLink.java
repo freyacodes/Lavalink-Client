d018c4b74d09589d315c7a78ffdbfb1447332896
Xirado <bquickzz0@gmail.com>
2022-08-14 10:43:12 +0200

    Add proper JDA 5.0.0-alpha.17 support

--- src/main/java/lavalink/client/io/jda/JdaLink.java
+++ src/main/java/lavalink/client/io/jda/JdaLink.java
@@ -6,10 +6,7 @@ import lavalink.client.io.GuildUnavailableException;
 import lavalink.client.io.Link;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.GuildVoiceState;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +21,8 @@ public class JdaLink extends Link {
         this.lavalink = lavalink;
     }
 
-    public void connect(@NonNull VoiceChannel voiceChannel) {
-        connect(voiceChannel, true);
+    public void connect(@NonNull AudioChannel audioChannel) {
+        connect(audioChannel, true);
     }
 
     /**
@@ -34,10 +31,10 @@ public class JdaLink extends Link {
      * @param channel Channel to connect to
      */
     @SuppressWarnings("WeakerAccess")
-    void connect(@NonNull VoiceChannel channel, boolean checkChannel) {
+    void connect(@NonNull AudioChannel channel, boolean checkChannel) {
         if (!channel.getGuild().equals(getJda().getGuildById(guild)))
-            throw new IllegalArgumentException("The provided VoiceChannel is not a part of the Guild that this AudioManager handles." +
-                    "Please provide a VoiceChannel from the proper Guild");
+            throw new IllegalArgumentException("The provided AudioChannel is not a part of the Guild that this AudioManager handles." +
+                    "Please provide an AudioChannel from the proper Guild");
         if (channel.getJDA().isUnavailable(channel.getGuild().getIdLong()))
             throw new GuildUnavailableException("Cannot open an Audio Connection with an unavailable guild. " +
                     "Please wait until this Guild is available to open a connection.");
@@ -52,8 +49,14 @@ public class JdaLink extends Link {
         if (checkChannel && channel.equals(voiceState.getChannel()))
             return;
 
-        if (voiceState.inVoiceChannel()) {
-            final int userLimit = channel.getUserLimit(); // userLimit is 0 if no limit is set!
+        if (voiceState.inAudioChannel()) {
+            int userLimit = 0; // userLimit is 0 if no limit is set!
+
+            if (channel instanceof VoiceChannel) {
+                VoiceChannel vc = (VoiceChannel) channel;
+                userLimit = vc.getUserLimit();
+            }
+
             if (!self.isOwner() && !self.hasPermission(Permission.ADMINISTRATOR)) {
                 if (userLimit > 0                                                      // If there is a userlimit
                         && userLimit <= channel.getMembers().size()                    // if that userlimit is reached
@@ -91,11 +94,11 @@ public class JdaLink extends Link {
 
     @Override
     protected void queueAudioConnect(long channelId) {
-        VoiceChannel vc = getJda().getVoiceChannelById(channelId);
-        if (vc != null) {
-            getJda().getDirectAudioController().connect(vc);
+        AudioChannel channel = getJda().getChannelById(AudioChannel.class, channelId);
+        if (channel != null) {
+            getJda().getDirectAudioController().connect(channel);
         } else {
-            log.warn("Attempted to connect, but voice channel {} was not found", channelId);
+            log.warn("Attempted to connect, but AudioChannel {} was not found", channelId);
         }
     }
 
